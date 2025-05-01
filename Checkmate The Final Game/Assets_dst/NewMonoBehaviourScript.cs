3261e0819a7cf9738f28a0f015f57b7a8977f755
David Ni <davidnihui@gmail.com>
2025-05-01 17:33:10 -0500

    Testing if it still is fine if it's a public repository

--- Checkmate The Final Game/Assets_dst/NewMonoBehaviourScript.cs
+++ Checkmate The Final Game/Assets_dst/NewMonoBehaviourScript.cs
@@ -9,7 +9,7 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //I made the repository public, just testing to make sure everything is still fine
     }
 
     // Update is called once per frame
