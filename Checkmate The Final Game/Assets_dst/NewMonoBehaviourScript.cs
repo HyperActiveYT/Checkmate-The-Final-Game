1176fda264e4b9b137e73333df607eb318b34ca7
David Ni <davidnihui@gmail.com>
2025-05-01 18:35:29 -0500

    .

--- Checkmate The Final Game/Assets_dst/NewMonoBehaviourScript.cs
+++ Checkmate The Final Game/Assets_dst/NewMonoBehaviourScript.cs
@@ -3,6 +3,7 @@ using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Checkmate_The_Final_Game.Chess_Mechanics;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
